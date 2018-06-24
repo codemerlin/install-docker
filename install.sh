be7f2119222f3f63bf140a6d18c1d97ca7fb7222
Mohit Thakral <me@mohitt.com>
2018-06-24 11:20:55 -0700

    Update install.sh
    removed adding the dockergroup latest version does that
    and removed creation of test-image

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash 
 
 curl -fsSL https://get.docker.com/ | sh
-sudo groupadd docker
 sudo usermod -aG docker $USER
-sudo systemctl restart docker
-# Test docker setup
-docker run --rm hello-world
 
